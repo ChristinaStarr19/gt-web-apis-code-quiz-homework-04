9e0fb88233e2b0be0bde6cfd1f53a9b5f87889d4
Christina Starr <christinastarr19@gmail.com>
2020-06-11 17:32:43 -0400

    Added event listeners for buttons and multiple functions:saveScores, endGame, renderQuestion

--- script.js
+++ script.js
@@ -1,26 +1,198 @@
- var timeLeft = 75;
+var timeLeft = 75;
 //This is how much time the user has to play the game.
-var timerEl = document.getElementById("#timer");
+var timerEl = document.getElementById("timer");
 //This references the element with the ID of timer in HTML.
-var startHere = document.getElementById("#startHere");
+var startHere = document.getElementById("startHere");
 //This references the button with the id of startHere in HTML.
-var welcome = document.getElementById("#welcome");
-var question1 = document.getElementById("#question1");
-//This referencfes the element with the ID of question1 in HTML.
+var welcome = document.getElementById("welcome");
+var questionSection = document.getElementById("question");
+//This references the element with the ID of question1 in HTML.
+startHere.addEventListener("click", begin);
+var questionIndex = 0;
+var timeInterval;
+var choiceEl;
+var responseEl;
+var initials;
+var submitEl;
 
-function begin(){
-    startHere.addEventListener("click", function() {
-    //When the user clicks on startHere, this funtion will begin.
-    welcome.style.display = "none";
-    question1.style.display = "block";
-    var timerInterval = setInterval(function() {
-        timeLeft--;
-        timerEl.textContent = "Quiz Time:" + timeLeft;
-        if (timeLeft === 0) {
-        clearInterval(timerInterval);
-        }
-    }, 1000);
- });
+var testContent = [
+  {
+    question: "Commonly Used data types DO NOT Include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question: "The condition in an if/ else statement is enclosed within ___.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parenthesis",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store __",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans**",
+      "all of the above",
+    ],
+    answer: "alerts",
+  },
+  {
+    question:
+      "Sting values must be enclosed within __ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "alerts",
+  },
+
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/ bash", "for loops", "console.log"],
+    answer: "alerts",
+  },
+];
+
+console.log("Hello World");
+function begin() {
+  welcome.setAttribute("style", "display:none");
+  timerEl.textContent = timeLeft + ":Seconds left";
+  timerStart();
+  renderQuestion();
 }
 
+function renderQuestion() {
+  if (questionIndex === 5) {
+    endGame();
+  }
 
+  //create QUESTION
+  questionSection.setAttribute("style", "display:block");
+  var questionEL = document.createElement("h3");
+  //add content
+  questionEL.textContent = testContent[questionIndex].question;
+  //append to existing element
+  questionSection.append(questionEL);
+  // create UNORDEREDLIST
+  var choiceList = document.createElement("div");
+  //add content
+  choiceList.textContent = "";
+  //append to existing element
+  questionSection.append(choiceList);
+  //create ANSWERCHOICE A
+  for (var i = 0; i < 4; i++) {
+    //create an element
+    choiceEl = document.createElement("li");
+    //choiceEl = document.createElement("button"); THey say I can just create a button instead.
+    //add content
+    choiceEl.textContent = testContent[questionIndex].choices[i];
+    //add style
+    choiceEl.setAttribute("type", "button");
+    //They say I don't need this.
+    choiceEl.setAttribute("class", "btn btn-primary mybutton btn-block");
+    //append to existing element
+    choiceList.append(choiceEl);
+  }
+  //create input
+  responseEl = document.createElement("p");
+  //add content
+  // responseEl.textContent = "";
+  //add two classes
+  responseEl.setAttribute("class", "correct incorrect");
+  //append to existing element
+  choiceList.append(responseEl);
+}
+questionSection.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (event.target.matches(".mybutton")) {
+    if (choiceEl.textContent !== testContent[questionIndex].answer) {
+      timeLeft = timeLeft - 10;
+      responseEl.textContent = "incorrect";
+    } else {
+      responseEl.textContent = "correct";
+    }
+    //give time for the message to show by adding a setInterval.
+
+    // if (choiceEl.textContent !== testContent[questionIndex].answer) {
+    //   timeLeft = timeLeft - 10;
+    //   responseEl.textContent = "incorrect";
+    // }
+    // else {
+    //   responseEl.textContent = "correct";
+    // }
+    var responseTime = setInterval(function () {
+      //Clear the HTML to start fresh
+      questionSection.innerHTML = "";
+      questionIndex++;
+      clearInterval(responseTime);
+      renderQuestion();
+    }, 1000);
+  }
+});
+//look at activity 18/19 for event delegation
+//when clicking on questionsection
+// check if element clicked is li
+// if textcontent === testContent[questionIndex].answer
+// else subtract time from timer
+// questionIndex++
+// call renderQuestion
+
+function timerStart() {
+  timeInterval = setInterval(function () {
+    timeLeft--;
+    timerEl.textContent = timeLeft + "Seconds left";
+    if (timeLeft <= 0) {
+      questionSection.innerHTML = "";
+      endGame();
+    }
+  }, 1000);
+}
+
+function endGame() {
+  //This will stop the timer.
+  clearInterval(timeInterval);
+  //This will clear the section and prepare it for the endGame screen.
+  questionSection.innerHTML = "";
+  //Create h3 to display initials question.
+  var questionEL = document.createElement("h3");
+  //add content
+  questionEL.textContent =
+    "You scored " +
+    timeLeft +
+    " points! If you would like to save this score, please type your intials below and click submit.";
+  //append to existing element
+  questionSection.append(questionEL);
+  //Create input field so that the initials can be saved.
+  var initialsEl = document.createElement("input");
+  //Content will be added later and saved to the local storage when submit button clicked.
+  //append to existing element
+  questionSection.append(initialsEl);
+  //Create list item element so that the button can be appended.
+  submitEl = document.createElement("li");
+  //choiceEl = document.createElement("button"); THey say I can just create a button instead.
+  //add content
+  submitEl.textContent = "Submit";
+  //add style
+  submitEl.setAttribute("type", "button");
+  submitEl.setAttribute(
+    "class",
+    "btn btn-primary mybutton btn-block submit-button"
+  );
+  //append to existing element
+  questionSection.append(submitEl);
+
+  //prompt user for initials
+  //display score (timeleft)
+}
+//Within this section, I want you to listen for a click from the .submit-button.
+questionSection.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (event.target.matches(".submit-button")) {
+    saveScores();
+  }
+});
+
+function saveScores() {
+  //save initialsEl in local storage
+  //save timeLeft in local storage
+  //This will clear the section and prepare it for the endGame screen.
+  questionSection.innerHTML = "";
+}
